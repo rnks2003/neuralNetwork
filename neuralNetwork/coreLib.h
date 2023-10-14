4445f880ba950c61d93f79a1d825aa4677d636cb
Ravi Narayana K S <rnks2003@gmail.com>
2023-10-14 22:26:19 +0530

    Structure of Input layer

--- neuralNetwork/coreLib.h
+++ neuralNetwork/coreLib.h
@@ -8,6 +8,18 @@
 #ifndef coreLib_h
 #define coreLib_h
 
-
+class layerINPUT {
+    float* values;
+    int neuronCount;
+public:
+    layerINPUT(int neurons){
+        this->neuronCount = neurons;
+        values = new float(neuronCount);
+    }
+    int readINPUT(){
+        std::cout<<"readINPUT : "<<neuronCount;
+        return (int)'s';
+    }
+};
 
 #endif /* coreLib_h */
