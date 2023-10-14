44e18cc2026826be178db30522146f2da80de7b8
Ravi Narayana K S <rnks2003@gmail.com>
2023-10-14 21:38:42 +0530

    Initial Commit

---
+++ neuralNetwork/coreLib.h
@@ -0,0 +1,13 @@
+//
+//  coreLib.h
+//  neuralNetwork
+//
+//  Created by Ravi Narayana K S on 14/10/23.
+//
+
+#ifndef coreLib_h
+#define coreLib_h
+
+
+
+#endif /* coreLib_h */
---
+++ neuralNetwork/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  neuralNetwork
+//
+//  Created by Ravi Narayana K S on 14/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
