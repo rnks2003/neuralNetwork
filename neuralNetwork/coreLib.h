0b3be0765d35e97ab18f1db0dc81c07e2055aeae
Ravi Narayana K S <rnks2003@gmail.com>
2023-10-20 15:30:06 +0530

    check if input values are latched to input  layer

--- neuralNetwork/coreLib.h
+++ neuralNetwork/coreLib.h
@@ -8,6 +8,8 @@
 #ifndef coreLib_h
 #define coreLib_h
 
+// class defining input layer
+
 class layerINPUT {
     float* values = NULL;
     
@@ -34,6 +36,19 @@ public:
         this->inputDim = 2;
     }
     int readStatus(){
+        /*   // check if all inputs are read
+             {
+                int i= 0;
+                float* temp = values;
+                do{
+                    std::cout<<std::endl;
+                    std::cout<<": "<<*temp;
+                    temp++;
+                    i++;
+                }while(i < xLen*yLen);
+                std::cout<<std::endl;
+            }
+        */
         return this->inputDim;
     }
 };
